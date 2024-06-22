fe9de241787c1114ad0230e0992851e6d065c9c1
Zenosaki <arselin.chaabanie@gmail.com>
2024-06-22 14:24:03 +0200

    Added Font awesome library

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,21 @@
-
+import { useState } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon as faSolidMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMoon as faRegularMoon } from '@fortawesome/free-regular-svg-icons';
 
 export default function Header(){
+  const [darkMode,setDarkMode]=useState(false)
+
+  const toggleDarkMode =()=>{
+    setDarkMode(!darkMode);
+  }
   return(
-    <></>
+    <div className="headerContainer Dark-Mode-On">
+      <h1>Where in the world?</h1>
+      <div className="Dark-Mode-Button" onClick={toggleDarkMode}>
+        <FontAwesomeIcon icon={darkMode ?faSolidMoon: faRegularMoon}/>
+        <p>Dark Mode</p>
+      </div>
+    </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
-
+import Header from '../components/header.jsx';
 
 
 export default function HomePage(){
   return(
-    <>
-    <h1>this is home page</h1>
-    </>
+    <div className="HomePage-Container">
+      <Header/>
+      <main>
+      </main>
+    </div>
   )
 }
