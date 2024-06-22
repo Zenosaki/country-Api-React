73a5563ac19755fa509e8f99c537608c56a770e7
Zenosaki <arselin.chaabanie@gmail.com>
2024-06-22 13:24:33 +0200

    Set upping the data base

--- src/DataBase/DataBase.js
+++
@@ -1,7 +0,0 @@
-let Countries=[
-{
-  
-},
-]
-
-export default Countries
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@
 
 export default function HomePage(){
   return(
-    <></>
+    <>
+    <h1>this is home page</h1>
+    </>
   )
 }
