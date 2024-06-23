eaf50e54b1248b6ec3351b6f8a5c8bb182accc0c
Zenosaki <arselin.chaabanie@gmail.com>
2024-06-23 15:26:05 +0200

    i finished the search bar style and dark mode fonctionnality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import Header from '../components/header.jsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import '../Sass/Main.scss';
 import { useState } from "react"
 
@@ -11,7 +13,22 @@ export default function HomePage(){
   return(
     <div className={`HomePage-Container ${darkMode ? 'Dark-Mode-On' : 'Dark-Mode-Off'}`}>
       <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
-      <div className={`Main-Content ${darkMode ? 'Main-DarkMode' : 'M Main-DarkMode-Off'}`}>
+      <div className={`Main-Content ${darkMode ? 'Main-DarkMode' : ' Main-DarkMode-Off'}`}>
+        <nav>
+
+          <div className={`SearchBar ${darkMode ? 'Dark-Mode-On' : 'Dark-Mode-Off'}`}>
+          <FontAwesomeIcon className='Search-Icon' icon={faMagnifyingGlass} />
+          <input type="text" placeholder='Search for a country...' />
+          </div>
+          <select className={`${darkMode ? 'Dark-Mode-On' : 'Dark-Mode-Off'}`}  name="Region" id="Reg">
+            <option value="" selected>Filter by Region</option>
+            <option value="Africa">Africa</option>
+            <option value="America">America</option>
+            <option value="Asia">Asia</option>
+            <option value="Europe">Europe</option>
+            <option value="Oceania">Oceania</option>
+          </select>
+        </nav>
       </div>
     </div>
   )
