25a06fc669129982fbfc71edcfea237541e4add3
Zenosaki <arselin.chaabanie@gmail.com>
2024-06-23 21:29:45 +0200

    i combined the filter methode in bothe the select and the search inputs(i made the search bar works with the select option)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,55 +2,76 @@ import Header from '../components/header.jsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import '../Sass/Main.scss';
-import { useState } from "react"
+import { useState } from 'react';
 import data from '../data.json';
-import Card from "../components/CountryCard.jsx";
+import Card from '../components/CountryCard.jsx';
 
-export default function HomePage(){
-  const [darkMode,setDarkMode]=useState(false)
+export default function HomePage() {
+  const [darkMode, setDarkMode] = useState(false);
   const [selectedRegion, setSelectedRegion] = useState('');
+  const [searchByName, setSearchByName] = useState('');
 
   const handleRegionChange = (event) => {
     setSelectedRegion(event.target.value);
   };
 
-  const filteredCountries = selectedRegion
-  ? data.countries.filter((country) => country.region === selectedRegion)
-  : data.countries;
-  const toggleDarkMode =()=>{
+  const handleSearchChange = (event) => {
+    setSearchByName(event.target.value);
+  };
+
+  const filteredCountries = data.countries.filter((country) => {
+    return (
+      (selectedRegion ? country.region === selectedRegion : true) &&
+      (searchByName ? country.name.toLowerCase().includes(searchByName.toLowerCase()) : true)
+    );
+  });
+
+  const toggleDarkMode = () => {
     setDarkMode(!darkMode);
-  }
-  return(
+  };
+
+  return (
     <div className={`HomePage-Container ${darkMode ? 'Dark-Mode-On' : 'Dark-Mode-Off'}`}>
-      <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
-      <div className={`Main-Content ${darkMode ? 'Main-DarkMode' : ' Main-DarkMode-Off'}`}>
+      <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <div className={`Main-Content ${darkMode ? 'Main-DarkMode' : 'Main-DarkMode-Off'}`}>
         <nav>
           <div className={`SearchBar ${darkMode ? 'Dark-Mode-On' : 'Dark-Mode-Off'}`}>
-          <FontAwesomeIcon className='Search-Icon' icon={faMagnifyingGlass} />
-          <input type="text" placeholder='Search for a country...' />
+            <FontAwesomeIcon className='Search-Icon' icon={faMagnifyingGlass} />
+            <input
+              onChange={handleSearchChange}
+              type='text'
+              placeholder='Search for a country...'
+            />
           </div>
-          <select onChange={handleRegionChange} className={`${darkMode ? 'Dark-Mode-On' : 'Dark-Mode-Off'}`}  name="Region" id="Reg">
-            <option value="" selected>Filter by Region</option>
-            <option value="Africa">Africa</option>
-            <option value="Americas">Americas</option>
-            <option value="Asia">Asia</option>
-            <option value="Europe">Europe</option>
-            <option value="Oceania">Oceania</option>
+          <select
+            onChange={handleRegionChange}
+            className={`${darkMode ? 'Dark-Mode-On' : 'Dark-Mode-Off'}`}
+            name='Region'
+            id='Reg'
+            value={selectedRegion}
+          >
+            <option value=''>Filter by Region</option>
+            <option value='Africa'>Africa</option>
+            <option value='Americas'>Americas</option>
+            <option value='Asia'>Asia</option>
+            <option value='Europe'>Europe</option>
+            <option value='Oceania'>Oceania</option>
           </select>
         </nav>
-        <div className="Countries-Cards-Section">
-        {filteredCountries.map((country,index) => (
-          <Card class={`${darkMode ? 'Dark-Mode-Cards' : 'Light-Mode-Cards'}`}
-           key={index} 
-           logo={country.flag}
-           Title={country.name} 
-           Population={country.population} 
-           Region={country.region} 
-           Capital={country.capital}
-           />
-        ))}
+        <div className='Countries-Cards-Section'>
+          {filteredCountries.map((country, index) => (
+            <Card
+              class={`${darkMode ? 'Dark-Mode-Cards' : 'Light-Mode-Cards'}`}
+              key={index}
+              logo={country.flag}
+              Title={country.name}
+              Population={country.population}
+              Region={country.region}
+              Capital={country.capital}
+            />
+          ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
