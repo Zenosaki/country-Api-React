69375bb9c263c02e5ea9014518c081ba06835a42
Zenosaki <arselin.chaabanie@gmail.com>
2024-06-25 17:43:12 +0200

    i deployed

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import InfoPage from './pages/Info.jsx';
 export default function App() {
   return (
     <div>
-    <BrowserRouter>
+    <BrowserRouter basename="/country-Api-React">
       <Routes>
         <Route index element={<HomePage/>}/>
         <Route path='/Home' element={<HomePage/>}/>
